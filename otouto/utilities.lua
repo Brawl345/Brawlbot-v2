862335dc776cb8d754a6116d32b9a0f363e47047
Andreas Bielawski <brawl98@outlook.com>
2016-07-02 12:35:14 +0200

    Implementation von Callback-Querys (auf ein neues!)

--- otouto/utilities.lua
+++ otouto/utilities.lua
@@ -52,11 +52,12 @@ end
 
 -- NOTE: Telegram currently only allows file uploads up to 50 MB
 -- https://core.telegram.org/bots/api#sendphoto
-function utilities:send_photo(chat_id, file, text, reply_to_message_id)
+function utilities:send_photo(chat_id, file, text, reply_to_message_id, reply_markup)
 	local output = bindings.request(self, 'sendPhoto', {
 		chat_id = chat_id,
 		caption = text or nil,
-		reply_to_message_id = reply_to_message_id
+		reply_to_message_id = reply_to_message_id,
+		reply_markup = reply_markup
 	}, {photo = file} )
 	os.remove(file)
 	print("Deleted: "..file)
@@ -78,11 +79,12 @@ function utilities:send_audio(chat_id, file, reply_to_message_id, duration, perf
 end
 
 -- https://core.telegram.org/bots/api#senddocument
-function utilities:send_document(chat_id, file, text, reply_to_message_id)
+function utilities:send_document(chat_id, file, text, reply_to_message_id, reply_markup)
 	local output = bindings.request(self, 'sendDocument', {
 		chat_id = chat_id,
 		caption = text or nil,
-		reply_to_message_id = reply_to_message_id
+		reply_to_message_id = reply_to_message_id,
+		reply_markup = reply_markup
 	}, {document = file} )
 	os.remove(file)
 	print("Deleted: "..file)
@@ -146,6 +148,15 @@ function utilities:send_typing(chat_id, action)
 	return bindings.request(self, 'sendChatAction', {
 		chat_id = chat_id,
 		action = action
+	} )
+end
+
+-- https://core.telegram.org/bots/api#answercallbackquery
+function utilities:answer_callback_query(callback, text, show_alert)
+	return bindings.request(self, 'answerCallbackQuery', {
+		callback_query_id = callback.id,
+		text = text,
+		show_alert = show_alert
 	} )
 end
 
