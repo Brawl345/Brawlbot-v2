e819ba4ded53eb72a1abfb820117b286747226e1
Andreas Bielawski <brawl98@outlook.com>
2016-07-23 13:24:43 +0200

    Patterns-Plugin von Otouto hinzugefügt

---
+++ otouto/plugins/patterns.lua
@@ -0,0 +1,33 @@
+local patterns = {}
+
+local utilities = require('otouto.utilities')
+
+patterns.triggers = {
+	'^/?s/.-/.-$'
+}
+
+function patterns:action(msg)
+	if not msg.reply_to_message then return true end
+	local output = msg.reply_to_message.text
+	if msg.reply_to_message.from.id == self.info.id then
+		output = output:gsub('Du meintest wohl:\n"', '')
+		output = output:gsub('"$', '')
+	end
+	local m1, m2 = msg.text:match('^/?s/(.-)/(.-)/?$')
+	if not m2 then return true end
+	local res
+	res, output = pcall(
+		function()
+			return output:gsub(m1, m2)
+		end
+	)
+	if res == false then
+		utilities.send_reply(self, msg, 'Falsches Pattern!')
+	else
+		output = output:sub(1, 4000)
+		output = 'Du meintest wohl:\n"' .. output .. '"'
+		utilities.send_reply(self, msg.reply_to_message, output)
+	end
+end
+
+return patterns
