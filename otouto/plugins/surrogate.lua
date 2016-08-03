e0f53dd20570d768defbf936d292e5b2dc8d4997
Andreas Bielawski <brawl98@outlook.com>
2016-08-03 16:58:02 +0200

    Surrogate nur für Sudoer

--- otouto/plugins/surrogate.lua
+++ otouto/plugins/surrogate.lua
@@ -6,7 +6,11 @@ surrogate.triggers = {
   "^/s (@[A-Za-z0-9-_-.-._.]+) +(.+)"
 }
 
-function surrogate:action(msg)
+function surrogate:action(msg, config, matches)
+  if not is_sudo(msg, config) then
+    utilities.send_reply(self, msg, config.errors.sudo)
+	return
+  end
   utilities.send_message(self, matches[1], matches[2], true, nil, true)
   return
 end
