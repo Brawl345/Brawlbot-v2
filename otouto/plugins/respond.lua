f62095634e79c5449704ce7f540cbe1eddef64f7
Andreas Bielawski <brawl98@outlook.com>
2016-08-11 17:03:23 +0200

    - Respond: Ändere URLs für Bilder, da GDrive-Webhosting Ende August eingestellt wird

--- otouto/plugins/respond.lua
+++ otouto/plugins/respond.lua
@@ -47,7 +47,7 @@ end
 function respond:action(msg, config, matches)
   local user_name = get_name(msg)
   local receiver = msg.chat.id
-  local GDRIVE_URL = 'https://de2319bd4b4b51a5ef2939a7638c1d35646f49f8.googledrive.com/host/0B_mfIlDgPiyqU25vUHZqZE9IUXc'
+  local BASE_URL = 'https://anditest.perseus.uberspace.de/plugins/respond'
   if user_name == "DefenderX" then user_name = "Deffu" end
 	
   if string.match(msg.text, "[Ff][Gg][Tt].? [Ss][Ww][Ii][Ff][Tt]") then
@@ -78,11 +78,11 @@ function respond:action(msg, config, matches)
     utilities.send_message(self, receiver,  '🐸🐸🐸')
     return
   elseif string.match(msg.text, "[Ii][Nn][Ll][Oo][Vv][Ee]") then
-    local file = download_to_file(GDRIVE_URL..'/inlove.gif')
+    local file = download_to_file(BASE_URL..'/inlove.gif')
     utilities.send_document(self, receiver, file)
     return
   elseif string.match(msg.text, "[Ww][Aa][Tt]") then
-    local WAT_URL = GDRIVE_URL..'/wat'
+    local WAT_URL = BASE_URL..'/wat'
     local wats = {
       "/wat1.jpg",
       "/wat2.jpg",
