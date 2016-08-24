4a9431ae1309025c7b46063cf17f3d56bd5f0ab1
Andreas Bielawski <brawl98@outlook.com>
2016-08-24 17:44:16 +0200

    Heise: Fix

--- otouto/plugins/heise.lua
+++ otouto/plugins/heise.lua
@@ -6,10 +6,10 @@ heise.triggers = {
 
 function heise:get_heise_article(article)
   local url = 'https://query.yahooapis.com/v1/public/yql?q=select%20content,src,strong%20from%20html%20where%20url=%22http://www.heise.de/newsticker/meldung/'..article..'.html%22%20and%20xpath=%22//div[@id=%27mitte_news%27]/article/header/h2|//div[@id=%27mitte_news%27]/article/div/p[1]/strong|//div[@id=%27mitte_news%27]/article/div/figure/img%22&format=json'
-  local res,code  = https.request(url)
+  local res, code  = https.request(url)
+  if code ~= 200 then return nil end
   local data = json.decode(res).query.results
-  if code ~= 200 then return "HTTP-Fehler" end
-  
+
   local title = data.h2
   if data.strong then
     teaser = '\n'..data.strong
@@ -31,6 +31,7 @@ end
 function heise:action(msg, config, matches)
   local article = URL.escape(matches[1])
   local text, image_url = heise:get_heise_article(article)
+  if not text then return end
   if image_url then
     utilities.send_typing(msg.chat.id, 'upload_photo')
     local file = download_to_file(image_url, 'heise_teaser.jpg')
